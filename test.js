5e91cbace4299ce8a88232a7cb4ab281ff1ead25
Umayange1998 <wgikumayanga@gmail.com>
2025-08-23 09:56:22 +0530

    For,continue

--- test.js
+++ test.js
@@ -1,39 +1,9 @@
-let a = "red";
-let b = "blue";
-
-let c = a;
-
-a = b;
-b = c;
-
-console.log(a);
-console.log(b);
-
-// Hour
-// Hour is between 6am and 12pm: Good morning
-// Hour is between 12pm and 4pm: Good Afternoon
-// Othervise Good Evening
-
-let Hour = 20;
-
-// if (Hour >= 6 && Hour < 12) {
-//   console.log("Good Morning");
-// } else if (Hour >= 12 && Hour < 16) {
-//   console.log("Good Afternoon");
-
-// } else{
-//     console.log("Good Evening")
-// }
-
-// let Role = "Guest";
-
-// switch (Role.toLowerCase()) {
-//   case "guest":
-//     console.log("Guest user");
-//     break;
-//   case "moderator":
-//     console.log("Moderator User");
-//     break;
-//   default:
-//     console.log("Unknown User");
-// }
+for(let i =0; i<10;){
+if(i%2 ===0){
+ i++;
+continue;
+}
+   
+console.log(i);
+i++;
+}
