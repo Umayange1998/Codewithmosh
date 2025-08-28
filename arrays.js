891744348c78cd9570290cb3624b8d8ca2e2e9f4
Umayange1998 <wgikumayanga@gmail.com>
2025-08-28 11:59:18 +0530

    Array Basic

---
+++ arrays.js
@@ -0,0 +1,36 @@
+
+
+// const numbers=[3,4,5,];
+
+// ///adding numbers
+// //End
+
+// numbers.push(6,7);
+
+// //Begining
+// numbers.unshift(1,2);
+
+// //Middle
+// numbers.splice(2,0,'a','b');
+
+// numbers.push(1);
+
+// console.log(numbers);
+
+// console.log(numbers.indexOf(5));
+// console.log(numbers.indexOf('1'));
+// console.log(numbers.indexOf(1));
+// console.log(numbers.lastIndexOf(1));
+
+// console.log(numbers.indexOf(2)!==-1);
+// console.log(numbers.includes(1));
+
+
+
+
+
+
+
+
+
+
